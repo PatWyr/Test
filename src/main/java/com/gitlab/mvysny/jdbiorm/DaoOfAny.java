55ac2f2b92d9c78bd3aa501ad303aef5b8b23539
Martin Vysny <martin@vysny.me>
2019-11-21 14:08:11 +0200

    minor

--- src/main/java/com/gitlab/mvysny/jdbiorm/DaoOfAny.java
+++ src/main/java/com/gitlab/mvysny/jdbiorm/DaoOfAny.java
@@ -40,7 +40,7 @@ public class DaoOfAny<T> {
     @NotNull
     public final Class<T> entityClass;  // public because of vok-orm
     @NotNull
-    protected final EntityMeta meta;
+    protected final EntityMeta meta;   // not public, since anybody can create instances of this class easily
     @NotNull
     protected final Helper<T> helper;  // not public, since anybody can create instances of this class easily
 
