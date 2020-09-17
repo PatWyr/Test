29a37bf286ec8c54f3d51517372da6982e5579e5
Martin Vysny <mavi@vaadin.com>
2020-09-17 20:22:38 +0300

    minor

--- build.gradle.kts
+++ build.gradle.kts
@@ -146,8 +146,3 @@ tasks.withType<Test> {
         showStandardStreams = true
     }
 }
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
