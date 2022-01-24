421c220b9fe536777eea6abe0e0d47d6d0b88fe3
Martin Vysny <mavi@vaadin.com>
2022-01-24 11:30:30 +0200

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     // workaround for https://github.com/google/gson/issues/1059
     testImplementation("com.fatboyindustrial.gson-javatime-serialisers:gson-javatime-serialisers:1.1.1")
     testImplementation("org.slf4j:slf4j-simple:$slf4jVersion")
-    testImplementation("com.h2database:h2:2.0.206")
+    testImplementation("com.h2database:h2:2.1.210")
 
     testImplementation("org.postgresql:postgresql:42.3.1")
     testImplementation("mysql:mysql-connector-java:8.0.25")
