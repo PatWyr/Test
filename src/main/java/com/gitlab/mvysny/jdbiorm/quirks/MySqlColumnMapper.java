64750062aabdb9e50e1906d317a8d5e3d27faa07
Martin Vysny <mavi@vaadin.com>
2022-11-24 10:30:35 +0200

    MariaDB workaround: UUID mapping: read bytes in case of BINARY/VARBINARY columns.

--- src/main/java/com/gitlab/mvysny/jdbiorm/quirks/MySqlColumnMapper.java
+++ src/main/java/com/gitlab/mvysny/jdbiorm/quirks/MySqlColumnMapper.java
@@ -3,12 +3,14 @@ package com.gitlab.mvysny.jdbiorm.quirks;
 import org.jdbi.v3.core.mapper.ColumnMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 import org.jetbrains.annotations.NotNull;
+import sun.reflect.generics.tree.TypeSignature;
 
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.UUID;
 
 /**
@@ -18,12 +20,20 @@ import java.util.UUID;
 public class MySqlColumnMapper implements ColumnMapper<UUID> {
 
     @Override
-    public UUID map(ResultSet r, int columnNumber, StatementContext ctx) throws SQLException {
+    public UUID map(@NotNull ResultSet r, int columnNumber, StatementContext ctx) throws SQLException {
+        final int columnType = r.getMetaData().getColumnType(columnNumber);
+        // MariaDB: if the column is defined as "id binary(16) primary key", the
+        // column type will be -3 VARBINARY!
+        if (columnType == Types.BINARY || columnType == Types.VARBINARY) {
+            final byte[] obj = r.getBytes(columnNumber);
+            return obj == null ? null : uuidFromByteArray(obj);
+        }
         final Object obj = r.getObject(columnNumber);
         if (obj == null) {
             return null;
         } else if (obj instanceof String) {
-            return UUID.fromString((String) obj);
+            final String str = (String) obj;
+            return UUID.fromString(str);
         } else if (obj instanceof byte[]) {
             return uuidFromByteArray((byte[]) obj);
         } else {
@@ -31,6 +41,7 @@ public class MySqlColumnMapper implements ColumnMapper<UUID> {
         }
     }
 
+    @NotNull
     private static UUID uuidFromByteArray(@NotNull byte[] uuid) {
         final DataInputStream din = new DataInputStream(new ByteArrayInputStream(uuid));
         try {
