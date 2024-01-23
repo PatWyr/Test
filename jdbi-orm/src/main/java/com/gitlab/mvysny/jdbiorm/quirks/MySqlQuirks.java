8b4e01bd046f8acbd112d9b2a653bc0f873b647e
Martin Vysny <mavi@vaadin.com>
2024-01-23 17:40:36 +0200

    MySqlQuirks: hot-patch missing LIMIT if OFFSET was specified

--- jdbi-orm/src/main/java/com/gitlab/mvysny/jdbiorm/quirks/MySqlQuirks.java
+++ jdbi-orm/src/main/java/com/gitlab/mvysny/jdbiorm/quirks/MySqlQuirks.java
@@ -4,6 +4,7 @@ import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.argument.Arguments;
 import org.jdbi.v3.core.mapper.ColumnMappers;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
@@ -16,5 +17,15 @@ public class MySqlQuirks implements Quirks {
     public void configure(@NotNull Handle handle) {
         handle.getConfig(Arguments.class).register(new MySqlUUIDArgumentFactory());
         handle.getConfig(ColumnMappers.class).register(UUID.class, new MySqlColumnMapper());
+    }
+
+    @Override
+    public String offsetLimit(@Nullable Long offset, @Nullable Long limit) {
+        if (offset != null && limit == null) {
+            // MySQL/MariaDB requires both OFFSET and LIMIT to be present in the SQL but only offset was set.
+            // hot-patch limit to some huge value.
+            limit = (long) Integer.MAX_VALUE;
+        }
+        return Quirks.super.offsetLimit(offset, limit);
     }
 }
