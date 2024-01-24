42e61a09faf94c1272eb0ce37b858493cb3ea3d4
Martin Vysny <mavi@vaadin.com>
2024-01-24 11:08:01 +0200

    2.6-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.gitlab.mvysny.jdbiorm"
-    version = "2.5"
+    version = "2.6-SNAPSHOT"
 
     repositories {
         mavenCentral()
